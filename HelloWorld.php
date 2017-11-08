7de3e5e8c2a04561e8ae15440ccaf460e8138f12
oktay aydın <oktayaydin6464@gmail.com>
2017-11-08 12:40:24 +0300

    first commit

---
+++ HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oktay
+ * Date: 08.11.2017
+ * Time: 11:30
+ */
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
